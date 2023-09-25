94ead8ce239141d5200df522300d36e534becff4
[MASTER] roshii <Roshii@stibits.io>
2023-09-25 14:06:14 -0400

    Build

---
+++ build/CMakeFiles/PDFtoImageConverter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PDFtoImageConverter.dir/src/main.cpp.o"
+  "CMakeFiles/PDFtoImageConverter.dir/src/main.cpp.o.d"
+  "PDFtoImageConverter"
+  "PDFtoImageConverter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PDFtoImageConverter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
